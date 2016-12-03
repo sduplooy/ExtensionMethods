6db841bfa83b33efd5b3a41cb0a2694f05eff89b
Sydney du Plooy <sduplooy@functioncore.com>
2016-12-03 10:14:33 +0200

    Initial commit.

---
+++ ParameterExtensions/ParameterExtensions.Tests/ParameterExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ParameterExtensions.Tests
+{
+	[TestFixture]
+	public class ParameterExtensionsTests
+	{
+		[Test]
+		public void Should_throw_argument_null_exception_when_value_is_null()
+		{
+			Assert.Throws<ArgumentNullException>(() => ParameterExtensions.ThrowIfNull(null, "paramName"));
+		}
+
+		[Test]
+		public void Should_throw_argument_null_exception_when_string_value_is_empty()
+		{
+			Assert.Throws<ArgumentNullException>(() => ParameterExtensions.ThrowIfNullOrEmpty(string.Empty, "paramName"));
+		}
+
+		[Test]
+		public void Should_throw_argument_null_exception_when_string_value_is_null()
+		{
+			Assert.Throws<ArgumentNullException>(() => ParameterExtensions.ThrowIfNullOrEmpty(null, "paramName"));
+		}
+	}
+}
---
+++ ParameterExtensions/ParameterExtensions/ParameterExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ParameterExtensions
+{
+	public static class ParameterExtensions
+	{
+		public static void ThrowIfNull(this object value, string parameterName)
+		{
+			if (value == null)
+				throw new ArgumentNullException(parameterName);
+		}
+
+		public static void ThrowIfNullOrEmpty(this string value, string parameterName)
+		{
+			if (string.IsNullOrEmpty(value))
+				throw new ArgumentNullException(parameterName);
+		}
+	}
+}
